0ad07ad4c0c0dc0600cda3c3e910b4f657288797
Durable Workflow <support@durable-workflow.com>
2026-04-03 18:40:49 +0000

    Revert CI worker capture instrumentation

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -43,17 +43,8 @@ abstract class TestCase extends BaseTestCase
 
         for ($i = 0; $i < self::NUMBER_OF_WORKERS; $i++) {
             self::$workers[$i] = new Process(['php', __DIR__ . '/../vendor/bin/testbench', 'queue:work']);
-            if (! self::shouldCaptureWorkerOutput()) {
-                self::$workers[$i]->disableOutput();
-                self::$workers[$i]->start();
-                continue;
-            }
-
-            file_put_contents(self::workerLogPath($i), '');
-
-            self::$workers[$i]->start(static function (string $type, string $output) use ($i): void {
-                file_put_contents(self::workerLogPath($i), $output, FILE_APPEND | LOCK_EX);
-            });
+            self::$workers[$i]->disableOutput();
+            self::$workers[$i]->start();
         }
     }
 
@@ -102,21 +93,5 @@ abstract class TestCase extends BaseTestCase
     protected function getPackageProviders($app)
     {
         return [\Workflow\Providers\WorkflowServiceProvider::class];
-    }
-
-    private static function shouldCaptureWorkerOutput(): bool
-    {
-        $value = getenv(
-            'WORKFLOW_TEST_CAPTURE_WORKER_OUTPUT'
-        ) ?: ($_ENV['WORKFLOW_TEST_CAPTURE_WORKER_OUTPUT'] ?? null);
-
-        return in_array($value, ['1', 'true', 'yes', 'on'], true);
-    }
-
-    private static function workerLogPath(int $worker): string
-    {
-        return dirname(
-            __DIR__
-        ) . '/vendor/orchestra/testbench-core/laravel/storage/logs/workflow-test-worker-' . $worker . '.log';
     }
 }
